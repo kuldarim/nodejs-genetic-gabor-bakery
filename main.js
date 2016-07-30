48846f70dfc8c53a3e3d61d5d67b49aac26d4dd1
Rimvydas Kulda <rimvydas.kulda@gmail.com>
2016-07-30 15:05:03 +0300

    introduce ES6 features

--- main.js
+++ main.js
@@ -3,20 +3,16 @@ var request = require('request');
 
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.get('/get-greyscale', function (req, res) {
+app.get('/get-greyscale', (req, res) => {
   // https://github.com/request/request - read more how to make simple http requests
-  request('http://localhost:8080/grayscale', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log('response get-greyscale received');
-    res.send(response);
-  }
-})
+  request('http://localhost:8080/grayscale', (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      console.log('response get-greyscale received');
+      res.send(response);
+    }
+  })
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
