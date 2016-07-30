ff9907ac5a907e332116df88e7806e6b3212e852
Rimvydas Kulda <rimvydas.kulda@gmail.com>
2016-07-30 16:18:44 +0300

    display requested image

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-var express = require('express');
-var request = require('request');
+const express = require('express');
+const request = require('request');
+var cv = require('opencv');
 
-var app = express();
+const app = express();
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
@@ -9,10 +10,30 @@ app.get('/get-greyscale', (req, res) => {
   // https://github.com/request/request - read more how to make simple http requests
   request('http://localhost:8080/grayscale', (error, response, body) => {
     if (!error && response.statusCode == 200) {
-      console.log('response get-greyscale received');
-      res.send(response);
+      const responseJson = JSON.parse(response.body)
+      const mat = convertJsonToMat(responseJson);
+      displayMat(mat);
+      res.send(responseJson);
     }
   })
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
+function convertJsonToMat(jsonObject) {
+  const rows = jsonObject.rows;
+  const cols = jsonObject.cols;
+  const type = jsonObject.type;
+  const data = new Buffer(jsonObject.data, 'base64');
+
+  let mat = new cv.Matrix(rows, cols, type);
+  mat.put(data);
+
+  return mat;
+}
+
+function displayMat(mat) {
+  const window = new cv.NamedWindow('Mat-Put', 0);
+  window.show(mat);
+  window.blockingWaitKey(0, 50);
+}
